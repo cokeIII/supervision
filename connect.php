65e384b9bccf450a1856a5d43acd8eb0389fc30f
cokeIII <coke2484@gmail.com>
2022-04-12 20:34:59 +0700

    f

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "root";
-$password = "";
+$password = "chontech2020!";
 $db = "supervision";
 $connect = mysqli_connect("$host","$user","$password","$db");
 $connect -> set_charset("utf8");
